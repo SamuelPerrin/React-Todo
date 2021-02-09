c16f2507654d603c469a64bfc9f107ab65ddcff4
SamuelPerrin <swperrin51@gmail.com>
2021-02-08 18:41:56 -0600

    get search working

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,11 +4,38 @@ import React from 'react';
 import Todo from './Todo';
 
 class TodoList extends React.Component {
-
+  constructor() {
+    super();
+    this.state = {
+      query: '',
+    };
+  }
+  
+  
+  
   render() {
+    let toShow = this.props.todos.filter(item => item.task.match(new RegExp(this.state.query, 'gi')));
+    
+    const handleChange = evt => {
+      this.setState({query: evt.target.value})
+    };
+
     return(
       <div>
-        {this.props.todos.map(item => <Todo key={item.task} task={item.task} completed={item.completed} toggleCheck={this.props.toggleCheck} />)}
+        <form onSubmit={e => {
+          e.preventDefault();
+          this.setState({query: ''})
+        }}
+        >
+          <input
+            type='text'
+            name='query'
+            value={this.state.query}
+            placeholder="Search..."
+            onChange={handleChange}
+          />
+        </form>
+        {toShow.map(item => <Todo key={item.task} task={item.task} completed={item.completed} toggleCheck={this.props.toggleCheck} />)}
       </div>
     )
   }
